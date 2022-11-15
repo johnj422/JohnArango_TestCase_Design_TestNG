b7dbefec07a47004cdd1ac8b5ffcd7484df2ce1b
john.arango <johnj42@gmail.com>
2022-11-15 13:27:52 -0500

    Driver class set

---
+++ src/main/java/org/espn/configuration/Driver.java
@@ -0,0 +1,27 @@
+package org.espn.configuration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Driver {
+
+    private WebDriver driver;
+
+    public Driver(String browser){
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+        }
+    }
+
+}
