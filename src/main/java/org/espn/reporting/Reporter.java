e5a498cd49157c0f7afb4d1c89c745e05448bf66
john.arango <johnj42@gmail.com>
2022-11-15 20:15:33 -0500

    Reporter class initialized

--- src/main/java/org/espn/reporting/Reporter.java
+++ src/main/java/org/espn/reporting/Reporter.java
@@ -1,4 +1,14 @@
 package org.espn.reporting;
 
+import org.tinylog.Logger;
 public class Reporter {
+    public Reporter() {
+    }
+
+    private static void info(String text) {
+        Logger.info(text);
+    }
+    private static void error(String text) {
+        Logger.error(text);
+    }
 }
